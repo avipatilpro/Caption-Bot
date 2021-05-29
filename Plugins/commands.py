a96ce23a736582d2e90be883bd16c54d9f705e47
Avishkar Patil <avishkarpatil29@gmail.com>
2021-05-29 10:45:07 +0530

    Initial Commit

---
+++ Plugins/commands.py
@@ -0,0 +1,215 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import pyrogram
+from pyrogram import filters
+from bot import autocaption
+from config import Config
+from translation import Translation
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+ 
+
+#all buttons 
+
+#start buttons 
+
+start_button=InlineKeyboardMarkup(
+        [
+              [
+                  InlineKeyboardButton("🖋 Current Caption", callback_data = "status_data")
+              ], 
+              [
+                  InlineKeyboardButton("🍃 Follow Me", url = "https://Instagram.com/proavipatil"), 
+                  InlineKeyboardButton("📕 About", callback_data = "about_data")
+              ], 
+              [
+                  InlineKeyboardButton("💡 Help", callback_data = "help_data"), 
+                  InlineKeyboardButton("🔐 Close", callback_data = "close_data")
+              ] 
+        ]
+)
+
+# help buttons
+
+help_button=InlineKeyboardMarkup(
+        [
+              [
+                InlineKeyboardButton("ABOUT MARKDOWN", callback_data = "markdown_data")
+              ], 
+              [
+                  InlineKeyboardButton("⏪ BACK", callback_data = "back_data"), 
+                  InlineKeyboardButton("🔐 CLOSE", callback_data = "close_data")
+              ]
+        ]
+) 
+
+# about button
+
+about_button=InlineKeyboardMarkup(
+        [
+              [
+                  InlineKeyboardButton("⬇️ BACK", callback_data = "back_data"), 
+                  InlineKeyboardButton("🔐 CLOSE", callback_data = "close_data")
+              ]
+        ]
+) 
+
+# Source Button
+
+source_button=InlineKeyboardMarkup(
+        [
+              [
+                  InlineKeyboardButton("⏪ Back", callback_data = "back_data"), 
+                  InlineKeyboardButton("🔐 Close", callback_data = "close_data")
+              ]
+        ]
+) 
+
+
+
+@autocaption.on_message(filters.command("start") & filters.private)
+async def start(bot, cmd):
+      await bot.send_message(
+          chat_id = cmd.chat.id,
+          text = Translation.START_TEXT.format(cmd.from_user.first_name, Config.ADMIN_USERNAME), 
+          reply_to_message_id = cmd.message_id,
+          parse_mode = "markdown",
+          disable_web_page_preview = True, 
+          reply_markup = start_button
+      )
+
+
+@autocaption.on_message(filters.command("help") & filters.private)
+async def help(bot, cmd):
+      await bot.send_message(
+          chat_id = cmd.chat.id,
+          text = Translation.HELP_TEXT, 
+          reply_to_message_id = cmd.message_id,
+          parse_mode = "html",
+          disable_web_page_preview = True,
+          reply_markup = help_button           
+      )
+
+
+@autocaption.on_message(filters.command("about") & filters.private)
+async def about(bot, cmd):
+      await bot.send_message(
+          chat_id = cmd.chat.id,
+          text = Translation.ABOUT_TEXT, 
+          reply_to_message_id = cmd.message_id,
+          parse_mode = "markdown",
+          disable_web_page_preview = True, 
+          reply_markup = about_button
+      )
+
+
+@autocaption.on_message(filters.command("source") & filters.private)
+async def about(bot, cmd):
+      await bot.send_message(
+          chat_id = cmd.chat.id,
+          text = Translation.SOURCE_TEXT, 
+          reply_to_message_id = cmd.message_id,
+          parse_mode = "html",
+          disable_web_page_preview = True, 
+          reply_markup = source_button
+      )      
+
+
+
+# call_backs 
+
+@autocaption.on_callback_query()
+async def button(bot, cmd: CallbackQuery):
+    cb_data = cmd.data
+    if "about_data" in cb_data:
+        await cmd.message.edit(
+             text = Translation.ABOUT_TEXT,
+             parse_mode="markdown", 
+             disable_web_page_preview=True, 
+             reply_markup=InlineKeyboardMarkup(
+                 [
+                     [
+                      InlineKeyboardButton("⬇️ BACK", callback_data="back_data"),
+                      InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
+                     ]
+ 
+                 ] 
+             ) 
+        )
+    elif "help_data" in cb_data:
+          await cmd.message.edit(
+               text=Translation.HELP_TEXT,
+               parse_mode="html", 
+               disable_web_page_preview=True, 
+               reply_markup=InlineKeyboardMarkup(
+                   [
+                       [
+                        InlineKeyboardButton("ABOUT MARKDOWN", callback_data = "markdown_data")
+                       ],
+                       [
+                        InlineKeyboardButton("⬇️ BACK", callback_data="back_data"),
+                        InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
+                       ]
+ 
+                   ] 
+               ) 
+          )
+    elif "back_data" in cb_data:
+          await cmd.message.edit(
+               text=Translation.START_TEXT.format(cmd.from_user.first_name, Config.ADMIN_USERNAME),
+               parse_mode="markdown", 
+               disable_web_page_preview=True, 
+               reply_markup=InlineKeyboardMarkup(
+                   [
+                      
+                       [
+                        InlineKeyboardButton("🖋 Current Caption", callback_data = "status_data")
+                       ], 
+                       [
+                        InlineKeyboardButton("🍃 Follow Me", url="https://Instagram.com/proavipatil"),
+                        InlineKeyboardButton("📕 About Me", callback_data="about_data")
+                       ],
+                       [
+                        InlineKeyboardButton("💡 Help", callback_data="help_data"),
+                        InlineKeyboardButton("🔐 Close", callback_data="close_data")
+                       ]
+                   ]
+               )
+          )
+    elif "close_data" in cb_data:
+          await cmd.message.delete()
+          await cmd.message.reply_to_message.delete()
+
+    elif "markdown_data" in cb_data:
+          await cmd.message.edit(
+               text=Translation.MARKDOWN_TEXT,
+               parse_mode="html", 
+               disable_web_page_preview=True, 
+               reply_markup=InlineKeyboardMarkup(
+                   [
+                       [
+                        InlineKeyboardButton("⬇️ BACK", callback_data="help_data"),
+                        InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
+                       ]
+ 
+                   ] 
+               ) 
+          )
+    elif "status_data" in cb_data:
+          await cmd.message.edit(
+               text=Translation.STATUS_DATA.format(Config.CAPTION_TEXT, Config.CAPTION_POSITION),
+               parse_mode="html", 
+               disable_web_page_preview=True, 
+               reply_markup=InlineKeyboardMarkup(
+                   [
+                       [
+                        InlineKeyboardButton("⬇️ BACK", callback_data="back_data"),
+                        InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
+                       ]
+ 
+                   ] 
+               ) 
+          )
+           
---
+++ README.md
@@ -0,0 +1,38 @@
+<h1 align='center'>🖊️ TG AutoCaption Bot </h1>
+
+
+<h4 align='center'>Bot Used For Auto Caption For Telegram Files & Video With Custom Text<br><br><i>(Only in channels either public or private Telegram channel)</i> </h4><br>
+
+
+<h4 align='center'>✯ Demo Bot ✯<br></h4>
+<h3 align='center' ><b><a href="https://telegram.me/CapXBot">¢αρтισи вσт</a></b></h3>
+
+
+###  Config Vars :
+
+- ```CAPTION_TEXT``` - Caption Below File [ Markdown ]
+
+- ```CAPTION_POSITION``` - Position of Your Caption According to File Name [ Top, Bottom, or Nil ]
+<br>
+
+### Deploy :
+
+- Fork This Repo
+- Without Any Changes Click Below Button 
+- Deploy This Repo
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+<br>
+
+
+### Commands :
+
+```
+/start - A command to start the bot
+/help  - A command to help you how to use the bot
+/about - A command to know about the bot
+```
+### Thanks :
+
+- [Avishkar Patil](https://github.com/avipatilpro)
+- [Ts_Bots](https://github.com/Ts-Bots)
