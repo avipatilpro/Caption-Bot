77bc527f4e2d972ea7d74fda74c0b3dd4eca409e
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 18:46:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,37 @@
-import pyrogram
-
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-import os
-
-from config import Config
-from pyrogram import Client 
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+from pyrogram import Client, enums, __version__
+from config import Config, LOGGER 
 
 class autocaption(Client):
-    
     def __init__(self):
         super().__init__(
-            session_name="Captioner",
-            bot_token = Config.BOT_TOKEN,
-            api_id = Config.API_ID,
-            api_hash = Config.API_HASH,
-            workers = 20,
-            plugins = dict(
-                root="Plugins"
-            )
+            "autocaption",
+            api_hash=Config.API_HASH,
+            api_id=Config.APP_ID,
+            bot_token=Config.BOT_TOKEN, 
+            workers=20,
+            plugins={
+                "root": "plugins"
+            }                        
+            
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        bot_details = await self.get_me()
+        self.set_parse_mode(enums.ParseMode.HTML)
+        self.LOGGER(__name__).info(
+            f"@{bot_details.username}  started! "
         )
 
-if __name__ == "__main__" :
-    autocaption().run()
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped. Bye.")
 
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+app = autocaption()
+app.run()
